dee95debc6fff0f23ffb811c17286c06fbc78e99
David Pallmann <42102018+davidpallmann@users.noreply.github.com>
2023-02-12 18:59:59 -0600

    Initial commit

---
+++ Functions.cs
@@ -0,0 +1,49 @@
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Annotations;
+using Amazon.Lambda.Annotations.APIGateway;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace HelloLambdaAnnotations
+{
+    /// <summary>
+    /// Sample Lambda functions that perform date functions.
+    /// </summary>
+    public class Functions
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Functions()
+        {
+        }
+
+        /// <summary>
+        /// Compute the difference in days between two dates, from and to.
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <returns>number of days between from and to.</returns>
+        [LambdaFunction()]
+        [HttpApi(LambdaHttpMethod.Get, "/datediff/{from}/{to}")]
+        public int DateDifferenceInDays(string from, string to, ILambdaContext context)
+        {
+            int days = Convert.ToInt16((DateTime.Parse(to) - DateTime.Parse(from)).TotalDays);
+            context.Logger.LogInformation($"datediff {from} {to} is {days}");
+            return days;
+        }
+
+        /// <summary>
+        /// Add a day count to a date and return the new date.
+        /// </summary>
+        /// <param name="date">starting date</param>
+        /// <param name="days">number of days to add</param>
+        /// <returns>resulting date</returns>
+        [LambdaFunction()]
+        [HttpApi(LambdaHttpMethod.Get, "/dateadd/{date}/{days}")]
+        public string DateAdd(string date, int days, ILambdaContext context)
+        {
+            return (DateTime.Parse(date).AddDays(days)).ToShortDateString();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Hello, Lambda Annotations!
+
+This episode: .NET Lambda Annotations Framework and simplified coding of HTTP endpoints. In this Hello, Cloud blog series, we're covering the basics of AWS cloud services for newcomers who are .NET developers. If you love C# but are new to AWS, or to this particular service, this should give you a jumpstart.
+
+In this post we'll introduce The .NET Lambda Annotations Framework and use it in a "Hello, Cloud" .NET program to create an API for date operations. We'll do this step-by-step, making no assumptions other than familiarity with C# and Visual Studio. We're using Visual Studio 2022 and .NET 6.
+In this post we'll introduce AWS App Runner and use it to host a simple .NET "Hello, Cloud" web app container. We'll do this step-by-step, making no assumptions other than familiarity with C# and Visual Studio. We're using Visual Studio 2022 and .NET 6.
+
+## Our Hello, Lambda Annotations Project
+
+In this tutorial, we'll create an API for date operations, defining several HTTP endpoints in one source file.
+
+See the blog post for the tutorial to create this project and run it on AWS.
+
